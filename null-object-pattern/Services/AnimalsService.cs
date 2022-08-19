8a8d25ee9c3ebea66ec9f0915c4125d004b510a0
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2022-08-19 21:07:02 +0100

    create services

---
+++ null-object-pattern/Services/AnimalsService.cs
@@ -0,0 +1,30 @@
+﻿using models;
+using System.Text;
+
+namespace Services
+{
+    public class AnimalsService
+    {
+        private readonly DataService _dataService;
+
+        public AnimalsService()
+        {
+            _dataService = new DataService();
+        }
+
+        public string GetInfo(string animalName)
+        {
+            Animal animal = _dataService.GetAnimal(animalName);
+
+            if (animal == Animal.Null)
+            {
+                return new StringBuilder()
+                    .Append($"We have no records for {animalName}.")
+                    .Append("Impossible. Perhaps the archives are incomplete.")
+                    .ToString();                
+            }
+
+            return animal.ToString();
+        }
+    }
+}
---
+++ null-object-pattern/Services/DataService.cs
@@ -0,0 +1,25 @@
+﻿using models;
+
+namespace Services
+{
+    public class DataService
+    {
+        private static readonly Dictionary<string, Animal> Animals = new()
+        {
+            [nameof(Cat)] = new Cat(),
+            [nameof(Chicken)] = new Chicken(),
+            [nameof(Cow)] = new Cow(),
+            [nameof(Dog)] = new Dog(),
+            [nameof(Eagle)] = new Eagle(),
+            [nameof(Frog)] = new Frog(),
+            [nameof(Lizard)] = new Lizard(),
+            [nameof(Snake)] = new Snake()
+        };
+
+        public Animal GetAnimal(string name)
+        {
+            Animals.TryGetValue(name, out Animal? animal);
+            return animal ?? Animal.Null;
+        }
+    }
+}
